54d597c37cf779e609595432f03fde9c089848ae
Rishona9 <96752903+Rishona9@users.noreply.github.com>
2022-08-28 12:57:48 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -239,7 +239,7 @@ form.addEventListener("submit", (e) => {
   const isZipValid = isValidZip(userZip);
   const userCvv = cvv.value;
   const isCvvValid = isValidCvv(userCvv);
-  const userActivity = selectedActivities.value;
+  const userActivity = selectedActivities.checked;
   const isActivityValid = isValidActivity(userActivity);
 
   if (!isNameValid) {
